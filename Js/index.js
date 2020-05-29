e0da09e0c3b1625d4bbf29488bd6c7805f86bcbf
Computadora de Juan <jzimmermann@udesa.edu.ar>
2020-05-29 19:13:43 -0300

    js

--- Js/index.js
+++ Js/index.js
@@ -7,3 +7,46 @@ ham.onclick = function(){
     ham.classList.toggle("change");
     nav.classList.toggle("nav-active")  
 }
+
+
+let topcharts = 'https://api.deezer.com/chart/0/tracks';
+
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+
+let url = proxy + topcharts;
+
+console.log(url);
+
+fetch(url)
+.then( function(respuesta){
+
+    return respuesta.json();
+    
+    })
+.then(function(datos){
+
+    console.log(datos)
+
+    let lista = document.querySelector('.charts-tracks');
+
+    cancion = datos.data;
+
+    
+    cancion.forEach(function(canciones){ 
+    
+      lista.innerHTML = '<p>'+ canciones.title + '</p> '
+
+            })
+
+            
+    
+         } )
+            
+
+.catch(function(error){
+
+    console.log(error);
+    
+})
+
+
