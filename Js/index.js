25857a7d9bde770b1a4ab8d0c2160b3c02b1b508
nachocavanagh <icavanaghnicholson@udesa.edu.ar>
2020-05-29 17:00:52 -0300

    burger

--- Js/index.js
+++ Js/index.js
@@ -1,11 +1,9 @@
 let ham = document.querySelector('.hamburger')
-let nav = document.querySelector('.nav')
+let nav = document.querySelector('.nav-links')
 let header = document.querySelector('header')
+let links = document.querySelectorAll('.nav-links div')
 
 ham.onclick = function(){
     ham.classList.toggle("change");
-    nav.style.display = 'flex'
-    
+    nav.classList.toggle("nav-active")  
 }
-
-
