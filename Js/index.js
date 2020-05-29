573699ab6034c2e9888b5cd2e5b8bba699cbe3f2
nachocavanagh <icavanaghnicholson@udesa.edu.ar>
2020-05-29 19:37:17 -0300

    Charts

--- Js/index.js
+++ Js/index.js
@@ -13,11 +13,10 @@ let topcharts = 'https://api.deezer.com/chart/0/tracks';
 
 let proxy = 'https://cors-anywhere.herokuapp.com/';
 
-let url = proxy + topcharts;
+let urlTracks = proxy + topcharts;
 
-console.log(url);
 
-fetch(url)
+fetch(urlTracks)
 .then( function(respuesta){
 
     return respuesta.json();
@@ -34,12 +33,77 @@ fetch(url)
     
     cancion.forEach(function(canciones){ 
     
-      lista.innerHTML = '<p>'+ canciones.title + '</p> '
+      lista.innerHTML = '<a href="detalle.html"><div class="item-charts"><p>'+ canciones.title + '</p></div></a>'
 
-            })
-
+            })   
+         } )
             
+
+.catch(function(error){
+
+    console.log(error);
     
+})
+
+
+//ALBUMS
+
+let urlAlbums = proxy + 'https://api.deezer.com/chart/0/albums'
+
+fetch(urlAlbums)
+.then( function(respuesta){
+
+    return respuesta.json();
+    
+    })
+.then(function(datos){
+
+    console.log(datos)
+
+    let lista2 = document.querySelector('.charts-albums');
+
+    cancion = datos.data;
+
+    
+    cancion.forEach(function(canciones){ 
+    
+      lista2.innerHTML = '<a href="detalle.html"><div class="item-charts"><p>'+ canciones.title + '</p></div></a>'
+
+            })   
+         } )
+            
+
+.catch(function(error){
+
+    console.log(error);
+    
+})
+
+
+//PLAYLISTS
+
+let urlPlaylists = proxy + 'https://api.deezer.com/chart/0/playlists'
+
+fetch(urlPlaylists)
+.then( function(respuesta){
+
+    return respuesta.json();
+    
+    })
+.then(function(datos){
+
+    console.log(datos)
+
+    let lista3 = document.querySelector('.charts-playlists');
+
+    cancion = datos.data;
+
+    
+    cancion.forEach(function(canciones){ 
+    
+      lista3.innerHTML = '<a href="detalle.html"><div class="item-charts"><p>'+ canciones.title + '</p></div></a>'
+
+            })   
          } )
             
 
