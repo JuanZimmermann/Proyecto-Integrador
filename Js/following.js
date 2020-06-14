3621d5284600f71eaa445f7077e7fd219581d8ca
horasbarbeito <hbarbeito@udesa.edu.ar>
2020-06-13 22:07:45 -0300

    test

--- Js/following.js
+++ Js/following.js
@@ -1,21 +1,25 @@
 let recuperaStorage = localStorage.getItem('artistList');
 
-let recuperoStorage = localStorage.getItem('playlistsFollowed');
+//let recuperoStorage = localStorage.getItem('playlistsFollowed');
 
 let artist = JSON.parse(recuperaStorage);
 
-let playlist = JSON.parse(recuperoStorage);
+//let playlist = JSON.parse(recuperoStorage);
 
 let artistWrapper = document.querySelector('.artlist')
 
+//let playlistWrapper = document.querySelector('.playlists')
+
 if(recuperaStorage == null || recuperaStorage == '[]'){
     artistList = []
-    artlistWrapper.innerHTML += '<h2> No sigues a ningun artista </h2>'
+    artistWrapper.innerHTML += '<h2> No sigues a ningun artista </h2>'
 }else{
-    artistList.forEach(function(artistId){
+    artist.forEach(function(artistId){
         buscarYMostrar(artistId)
     })
 }
+
+
 
 function buscarYMostrar(artistId){
 
@@ -29,7 +33,7 @@ var url = proxy + 'https://api.deezer.com/track/' + artistId;
         })
         .then(function(track){
     
-            artlistwrapper.innerHTML += '<li><h3>'+ artistId +'</h3></li>'
+            artistWrapper.innerHTML += '<li><h3>'+ artistId +'</h3></li>'
 
             console.log(artistId);
 
@@ -43,6 +47,42 @@ var url = proxy + 'https://api.deezer.com/track/' + artistId;
     
      }
     
+
+    /* if(recuperoStorage == null || recuperoStorage == '[]'){
+        playlistsFollowed = []
+        playlistWrapper.innerHTML += '<h2> No sigues a ninguna playlist </h2>'
+    }else{
+        playlist.forEach(function(playlistId){
+            buscarYMostrar(playlistId)
+        })
+    }
+
+     function buscarYMostrar(playlistId){
+
+        var proxy = 'https://cors-anywhere.herokuapp.com/'
+        var url = proxy + 'https://api.deezer.com/track/' + playlistId;
+            
+                fetch(url)
+                 .then( function(response){
+                     return response.json();
+                
+                })
+                .then(function(track){
+            
+                    playlistWrapper.innerHTML += '<li><h3>'+ playlistId +'</h3></li>'
+        
+                    console.log(playlistId);
+        
+                   })
+                   
+            
+            .catch(function(error){
+                console.log(error);
+                
+            })
+            
+             }*/
+
      console.log(localStorage);
      
      
